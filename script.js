02fdbd0c17b9729350f7096881be07a709cd1c90
Ashwin Wilson <ashwinwilson24@gmail.com>
2024-03-09 09:01:10 +0530

    code minified

--- script.js
+++ script.js
@@ -1,52 +1,12 @@
 import "https://cdn.jsdelivr.net/npm/typeit@7.0.0/dist/typeit.min.js";
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.contact-slide').height('0')
-   $('.front-slide').slideUp(5000)
-   $(".left-slide").width("0")
-   typeAnimate();
+    $('.front-slide').slideUp(5000)
+    $(".left-slide").width("0")
+    typeAnimate();
 });
 
-function typeAnimate(){
-    setTimeout(() => {
-        // console.log('sleeping')
-        
-        new TypeIt(".title", {
-            speed: 50,
-            loop: false
-        }).go()
-       }, 2000);
-           
-}
-new TypeIt(".typeAnimation", {
-    speed: 25,
-    loop: false
-}).go();
+function typeAnimate() { setTimeout(() => { new TypeIt(".title", { speed: 50, loop: !1 }).go() }, 2e3) } new TypeIt(".typeAnimation", { speed: 25, loop: !1 }).go(); const observerOptions = { root: null, rootMargin: "0px", threshold: 1 }, aboutPage = document.getElementById("about"), aboutSlideRight = document.getElementById("aboutDP"), aboutSlideLeft = document.getElementById("about-box"), aboutObserver = new IntersectionObserver(function (e, t) { e.forEach(e => { e.isIntersecting && (aboutSlideRight.classList.add("animateRight"), aboutSlideLeft.classList.add("animateLeft")) }) }, observerOptions); aboutObserver.observe(aboutPage);
 
-const observerOptions = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 1.0
-};
-const aboutPage = document.getElementById('about');
-const aboutSlideRight = document.getElementById('aboutDP');
-const aboutSlideLeft = document.getElementById('about-box');
-const aboutObserver = new IntersectionObserver(function (entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            // console.log(entries)
-            aboutSlideRight.classList.add('animateRight');
-            aboutSlideLeft.classList.add('animateLeft');
-        }
-        // else {
-        //     aboutSlideRight.classList.remove('animateRight');
-        // }
-    });
-}, observerOptions);
 
-aboutObserver.observe(aboutPage);
-
-// new TypeIt(".resumeType", {
-//     speed: 20,
-//     loop: false
-// }).go();
