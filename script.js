8cd4a79c13566e50c03b96528477ade06036139a
Ashwin Wilson <ashwinwilson24@gmail.com>
2024-03-09 08:51:30 +0530

    animation entry slideUp animation added and some design faults solved

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 import "https://cdn.jsdelivr.net/npm/typeit@7.0.0/dist/typeit.min.js";
 
-new TypeIt(".title", {
-    speed: 50,
-    loop: false
-}).go()
+$(document).ready(function(){
+    $('.contact-slide').height('0')
+   $('.front-slide').slideUp(5000)
+   $(".left-slide").width("0")
+   typeAnimate();
+});
 
+function typeAnimate(){
+    setTimeout(() => {
+        // console.log('sleeping')
+        
+        new TypeIt(".title", {
+            speed: 50,
+            loop: false
+        }).go()
+       }, 2000);
+           
+}
+new TypeIt(".typeAnimation", {
+    speed: 25,
+    loop: false
+}).go();
 
 const observerOptions = {
     root: null,
@@ -14,17 +31,12 @@ const observerOptions = {
 const aboutPage = document.getElementById('about');
 const aboutSlideRight = document.getElementById('aboutDP');
 const aboutSlideLeft = document.getElementById('about-box');
-
 const aboutObserver = new IntersectionObserver(function (entries, observer) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             // console.log(entries)
             aboutSlideRight.classList.add('animateRight');
             aboutSlideLeft.classList.add('animateLeft');
-            new TypeIt(".typeAnimation", {
-                speed: 20,
-                loop: false
-            }).go();
         }
         // else {
         //     aboutSlideRight.classList.remove('animateRight');
@@ -34,7 +46,7 @@ const aboutObserver = new IntersectionObserver(function (entries, observer) {
 
 aboutObserver.observe(aboutPage);
 
-new TypeIt(".resumeType", {
-    speed: 20,
-    loop: false
-}).go();
+// new TypeIt(".resumeType", {
+//     speed: 20,
+//     loop: false
+// }).go();
