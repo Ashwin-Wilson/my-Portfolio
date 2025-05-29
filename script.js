9b39edceccb9e2417b9fc8d81b26cca284a14759
Ashwin-Wilson <ashwinwilson24@gmail.com>
2025-05-29 23:46:38 +0530

    glow effect on hover added resume section redesigned

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-import "https://cdn.jsdelivr.net/npm/typeit@7.0.0/dist/typeit.min.js";
+// import "https://cdn.jsdelivr.net/npm/typeit@7.0.0/dist/typeit.min.js";
 
-$(document).ready(function () {
-  $(".contact-slide").height("0");
-  $(".front-slide").slideUp(3000);
-  $(".left-slide").width("0");
-  typeAnimate();
-});
+// $(document).ready(function () {
+//   $(".contact-slide").height("0");
+//   $(".front-slide").slideUp(3000);
+//   $(".left-slide").width("0");
+//   typeAnimate();
+// });
 
-function typeAnimate() {
-  setTimeout(() => {
-    new TypeIt(".title", { speed: 50, loop: !1 }).go();
-  }, 2e3);
-}
-new TypeIt(".typeAnimation", {
-  speed: 25,
-  loop: !1,
-}).go();
+// function typeAnimate() {
+//   setTimeout(() => {
+//     new TypeIt(".title", { speed: 50, loop: !1 }).go();
+//   }, 2e3);
+// }
+// new TypeIt(".typeAnimation", {
+//   speed: 25,
+//   loop: !1,
+// }).go();
